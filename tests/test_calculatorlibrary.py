ff2c9a9e242026cbd8fa123eebcbac724137b4ee
plesz <r.plesz@yahoo.de>
2022-05-14 21:50:40 +0200

    Add test for add function.

--- tests/test_calculatorlibrary.py
+++ tests/test_calculatorlibrary.py
@@ -1,5 +1,10 @@
 from calculatorlibrary import __version__
+from calculatorlibrary.arithmetics import add
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+class TestCalculator:
+    def test_version(self):
+        assert __version__ == "0.1.0"
+
+    def test_add_function(self, args=[2, 3, 4, 5, 6]):
+        assert add(*args) == 20
