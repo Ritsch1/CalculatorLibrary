eb92616946e559b7d16d2b731485f9113eb734e9
plesz <r.plesz@yahoo.de>
2022-05-30 23:16:52 +0200

    Use pytest fixture for providing/reusing test data.

--- tests/test_calculatorlibrary.py
+++ tests/test_calculatorlibrary.py
@@ -1,10 +1,23 @@
 from calculatorlibrary import __version__
 from calculatorlibrary.arithmetics import add
+import pytest
 
 
 class TestCalculator:
+    # Follow DRY pattern by providing and reusing test data for multiple functions
+    @pytest.fixture
+    def sample_numeric_data(self):
+        return {
+            "Addition_Subtraction": [
+                ((1, 1, 2), 4, -2),
+                ((2, 78, 43), 123, -119),
+                ((-2, 10, 22), 30, -14),
+            ]
+        }
+
     def test_version(self):
         assert __version__ == "0.1.0"
 
-    def test_add_function(self, args=[2, 3, 4, 5, 6]):
-        assert add(*args) == 20
+    def test_add_function(self, sample_numeric_data):
+        for args, result, _ in sample_numeric_data["Addition_Subtraction"]:
+            assert add(*args) == result
